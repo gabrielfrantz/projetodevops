ac6390090711687b2e0a08ed0cf138ea90c77d56
gabrielfrantz <gabrieldecarvalhofrantz@gmail.com>
2023-09-05 17:36:51 -0300

    application start

--- src/application/cadastro.py
+++ src/application/cadastro.py
@@ -1,1 +1,18 @@
 # projeto devops
+# cadastros de e-mail
+# permitir visualizar todos os e-mails cadastrados no banco de dados
+
+def menu():
+    opcao=input('Bem-vindo!\nEscolha uma opção:\n1-Cadastrar e-mail\n2-Listar e-mails cadastrados\n3-Sair\n')
+    return int(opcao)
+
+
+while True:
+    opcao = menu()
+    if opcao == 1:
+        print("Cadastrar e-mail!")
+    elif opcao == 2:
+        print("Listar e-mails!")
+    else:
+        print("Saindo...")
+        break
